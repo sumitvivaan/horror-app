d06b8c82bb4c1d5f562cdc1197a4d5cffee32484
sumitvivaan <vivaan2024koshiya@gmail.com>
2026-08-24 18:04:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const UPLOAD_PRESET = "wlse6ksh";
 // ⬇️ LINE 11: RAZORPAY KEY ⬇️
 const RAZORPAY_KEY = "rzp_test_TSLL3jml0siRz4";
 const AMBIENCE_URL = "https://res.cloudinary.com/zyexm5wm/video/upload/v1787307374/simplesound-horror-trailer-443327.mp3";
-const VAPID_PUBLIC_KEY = "BDIFBrmb92vnSOX52kfrlZroOkWmw2-a6dZ3Y8O0nl0mnNMw3S3u3Cue-cUs5q-HA36y6nrjG2VdmDJa6JzuRvM",
+const VAPID_PUBLIC_KEY = "BDIFBrmb92vnSOX52kfrlZroOkWmw2-a6dZ3Y8O0nl0mnNMw3S3u3Cue-cUs5q-HA36y6nrjG2VdmDJa6JzuRvM";
 
 const CATEGORIES = ['चुड़ैल', 'हवेली', 'श्मशान', 'आपबीती', 'जंगल', 'अन्य'];
 
